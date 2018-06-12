8ce55389166d7726e8a29522aae0c0f57fd70402
Vinayak Sharda <vinayak.sharda1994.com>
2018-06-12 23:50:41 +0530

    Jobs are showing up

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -9,4 +9,5 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.homepage, name='home'),
+    path('', blog.views.homepage, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Job
 
 
 def homepage(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {"jobs": jobs})
