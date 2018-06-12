ea9b3220a8c2bce135fbde706c35b1d7f3048a3e
Vinayak Sharda <vinayak.sharda1994.com>
2018-06-12 23:19:23 +0530

    Comitting to Remote

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255, default='Hi')
+    pub_date = models.DateTimeField()
+    body = models.TextField(default='Hi')
+    image = models.ImageField(upload_to="images/")
+
--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Job(models.Model):
 
-    image = models.ImageField(upload_to='')
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
